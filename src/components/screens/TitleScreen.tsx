5a9dc5254e6bb3998fbd180f840eadfda51fd195
Christian-ctrl-cmd <christiangraham519@gmail.com>
2025-06-08 20:31:58 -0400

    Fix title screen background with working film image

--- src/components/screens/TitleScreen.tsx
+++ src/components/screens/TitleScreen.tsx
@@ -19,7 +19,7 @@ const TitleScreen = () => {
       exit={{ opacity: 0 }}
       className="min-h-screen w-full bg-black flex flex-col items-center justify-center relative overflow-hidden"
     >
-      <div className="absolute inset-0 bg-[url('https://images.pexels.com/photos/390089/pexels-photo-390089.jpeg')] bg-cover bg-center opacity-20" />
+      <div className="absolute inset-0 bg-[url('https://images.pexels.com/photos/274937/pexels-photo-274937.jpeg')] bg-cover bg-center opacity-20" />
       
       <motion.div
         initial={{ y: -50, opacity: 0 }}
